517004c40bffcb8dd53a4e20c280e6c9e61974ea
Chris Janik <chris@ifttt.com>
2018-05-01 16:39:18 -0700

    stringify props

--- lib/react/server_rendering/node_js_renderer.rb
+++ lib/react/server_rendering/node_js_renderer.rb
@@ -12,6 +12,7 @@ module React
       end
 
       def render(component_name, props, prerender_options)
+        props = props.to_json
         HTTParty.get(@node_server_url, query: { component_name: component_name, props: props }).to_s
       rescue ExecJS::ProgramError => err
         raise React::ServerRendering::PrerenderError.new(component_name, props, err)
