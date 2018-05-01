8517768368149cbf2606a307fbd3c87894aaec94
Chris Janik <chris@ifttt.com>
2018-05-01 15:40:35 -0700

    add node js renderer class

---
+++ lib/react/server_rendering/node_js_renderer.rb
@@ -0,0 +1,21 @@
+module React
+  module ServerRendering
+
+    # This renderer class makes a request to an external node service.
+    # Allows you to stub out a browser environment in node.js so you can render react components
+    # that have dependencies on window/document/jQuery etc
+    # Assumes all assets are loaded by the node server.
+    class NodeJSRenderer
+      # @context is not available using this class
+      def initialize(options={})
+        @node_server_url = options.fetch(:node_server_url, '')
+      end
+
+      def render(component_name, props, prerender_options)
+        HTTParty.get(@node_server_url, query: { component_name: component_name, props: props }).to_s
+      rescue ExecJS::ProgramError => err
+        raise React::ServerRendering::PrerenderError.new(component_name, props, err)
+      end
+    end
+  end
+end
