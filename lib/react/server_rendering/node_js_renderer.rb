6383930d26a41b2af1ef2ad4f3513a64ef11ffc7
Chris Janik <chris@ifttt.com>
2018-07-16 16:38:39 -0700

    rearrange

--- lib/react/server_rendering/node_js_renderer.rb
+++ lib/react/server_rendering/node_js_renderer.rb
@@ -1,6 +1,5 @@
 module React
   module ServerRendering
-
     # This renderer class makes a request to an external node service.
     # Allows you to stub out a browser environment in node.js so you can render react components
     # that have dependencies on window/document/jQuery etc
@@ -8,21 +7,16 @@ module React
     class NodeJSRenderer
       # @context is not available using this class
       def initialize(options={})
-        @uri = URI options.fetch(:node_server_url, '')
+        require 'net/http/persistent'
+
+        @uri = URI(options.fetch(:node_server_url, ''))
         @http = Net::HTTP::Persistent.new 'server_renderer'
       end
 
       def render(component_name, props, prerender_options)
         props = props.to_json
-        # HTTParty.get(@node_server_url, query: { component_name: component_name, props: props }).to_s
-
-        response = (@http.request @uri, query => { component_name: component_name, props: props }).to_s
-
-        # if you are done making http requests, or won't make requests for several
-        # minutes
-        # http.shutdown
-
-
+        @uri.query = URI.encode_www_form({ :component_name => component_name, :props => props })
+        @http.request(@uri).to_s
       rescue ExecJS::ProgramError => err
         raise React::ServerRendering::PrerenderError.new(component_name, props, err)
       end
