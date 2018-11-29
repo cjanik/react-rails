0ff172f5a0a5ad7b904bef33f4f0f08270b22d7d
Chris Janik <chris@ifttt.com>
2018-11-29 10:48:11 -0800

    clean up logging

--- lib/react/server_rendering/node_js_renderer.rb
+++ lib/react/server_rendering/node_js_renderer.rb
@@ -7,32 +7,19 @@ module React
     # that have dependencies on window/document/jQuery etc
     # Assumes all assets are loaded by the node server.
     class NodeJSRenderer
-      def logger
-        @@logger ||= defined?(Rails.logger) ? Rails.logger : Logger.new(STDOUT)
-      end
       # @context is not available using this class
       def initialize(options={})
         @uri = URI(options.fetch(:node_server_url, ''))
-        @http = Net::HTTP::Persistent.new name: 'server_renderer'
-        logger.info "initialized Net::HTTP connection"
+        @http = Net::HTTP::Persistent.new(name: 'server_renderer')
       end
 
       def render(component_name, props, prerender_options)
-        logger.info "prerendering #{component_name}"
-        props = props.to_json
-        # @uri.query = URI.encode_www_form({ :component_name => component_name, :props => props })
-        # resp = @http.request(@uri)
-
-        post = Net::HTTP::Post.new @uri.path
+        post = Net::HTTP::Post.new(@uri.path)
         post['Content-Type'] = 'application/json'
-        post.set_form_data "component_name" => component_name, "props" => props
-        resp = @http.request @uri, post
-
-        logger.info resp
-        resp.body
-      rescue => err
-        logger.info err
-        raise React::ServerRendering::PrerenderError.new(component_name, props, err)
+        post.set_form_data(component_name: component_name, props: props.to_json)
+        @http.request(@uri, post).body
+      rescue Net::HTTP::Persistent::Error => err
+        React::ServerRendering::PrerenderError.new(component_name, props, err)
       end
     end
   end
